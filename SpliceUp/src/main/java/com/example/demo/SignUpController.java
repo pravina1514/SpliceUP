e120779e786aeb3c00ee3c902f9b989278439e29
HP <HP@DESKTOP-QU6GQ9B>
2019-02-07 20:09:41 +0530

    signup controller

--- SpliceUp/src/main/java/com/example/demo/SignUpController.java
+++ SpliceUp/src/main/java/com/example/demo/SignUpController.java
@@ -7,7 +7,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,26 +24,12 @@ public class SignUpController {
 	@Autowired
 	UserService service;
 
-	@Autowired
-	private SecurityService securityService;
-
-	@RequestMapping(value = "/createuser", method = RequestMethod.POST)
-	public String registration(@ModelAttribute Login user) {
-		user.getUserDetail().setUser(user);
-		Login dbUser = service.saveUser(user);
-
-		securityService.autologin(dbUser.getEmail(), dbUser.getPassword());
-
-		return "redirect:/signup/home2";
-	}
-
-	@GetMapping
-	public ModelAndView home2() {
+	@GetMapping(value = "/contact")
+	public ModelAndView contact() {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("contact");
 		return modelAndView;
 	}
-<<<<<<< HEAD
 
 	@GetMapping(value = "/home1")
 	public ModelAndView home1() {
@@ -59,11 +44,14 @@ public class SignUpController {
 		modelAndView.setViewName("home2");
 		return modelAndView;
 	}
-=======
->>>>>>> branch 'master' of https://github.com/rasheee03/spliceup.git
 
+	@GetMapping(value = "/events")
+	public ModelAndView events() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("services");
+		return modelAndView;
+	}
 
-<<<<<<< HEAD
 	@GetMapping(value = "/loginuser")
 	public ModelAndView inside(Map<String, Object> model) {
 		Login user = new Login();
@@ -86,16 +74,21 @@ public class SignUpController {
 
 	@GetMapping
 	public ModelAndView welcome(Map<String, Object> model) {
-=======
-
-	@RequestMapping(value = "/registration", method = RequestMethod.GET)
-	public ModelAndView registration(Model model) {
->>>>>>> branch 'master' of https://github.com/rasheee03/spliceup.git
 		Login user = new Login();
 		user.setUserDetail(new UserDetail());
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("registeration");
 		modelAndView.addObject("user", user);
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/createuser", method = RequestMethod.POST)
+	public ModelAndView save(@ModelAttribute Login user) {
+		user.getUserDetail().setUser(user);
+		Login dbUser = service.saveUser(user);
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("signup");
+		modelAndView.addObject("user", dbUser);
 		return modelAndView;
 	}
 
