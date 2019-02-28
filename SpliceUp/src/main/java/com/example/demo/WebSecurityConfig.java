2c5d05a6cb9627f60742a67dc4ee14c13b9b3666
HP <HP@DESKTOP-QU6GQ9B>
2019-02-28 19:47:28 +0530

    SERVICES

--- SpliceUp/src/main/java/com/example/demo/WebSecurityConfig.java
+++ SpliceUp/src/main/java/com/example/demo/WebSecurityConfig.java
@@ -24,9 +24,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/images/*", "/webfonts/*", "/resources/**", "/css/*", "/js/*", "/login/*")
-				.permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and()
-				.logout().permitAll();
+		http.authorizeRequests().antMatchers("/admin/*").access("hasRole('ROLE_ADMIN')")
+				.antMatchers("/images/*", "/webfonts/*", "/resources/**", "/css/*", "/js/*", "/login/*").permitAll()
+				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+				.permitAll();
 	}
 
 	@Autowired
