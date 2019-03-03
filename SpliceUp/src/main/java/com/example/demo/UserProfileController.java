71930fecdf47db989ba5b2af43f448f84455e03b
lenovo <lenovo@192.168.0.20>
2019-03-03 13:17:20 +0530

    profile complete

--- SpliceUp/src/main/java/com/example/demo/UserProfileController.java
+++ SpliceUp/src/main/java/com/example/demo/UserProfileController.java
@@ -2,7 +2,11 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,6 +16,10 @@ public class UserProfileController {
 	
 	@Autowired
 	UserService service;
+	
+	@Autowired
+	UserMasterRepository userRepo;
+
 	
 	
 	
@@ -25,6 +33,31 @@ public class UserProfileController {
 		
 
 		return modelAndView;
+	}
+
+	
+	
+
+	@PostMapping(value = "/updateuser")
+	public String updateuser(@ModelAttribute Login user) {
+		//Login existingUser = service.findUserByEmail(user.getEmail());
+		//Login existingUser =service.getLoggedInUser();
+		
+		//user.setUserDetail(service.getLoggedInUser());
+		user.getUserDetail().setLogin(user);
+		userRepo.save(user);
+		
+		
+		
+		//Login user = new Login();
+		//user.setUserDetail(new UserDetail());
+		//ModelAndView modelAndView = new ModelAndView();
+		
+		//modelAndView.setViewName("profile");
+		//modelAndView.addObject("user", service.getLoggedInUser());
+		
+
+		return "redirect:/profile/userProfile";
 	}
 
 }
